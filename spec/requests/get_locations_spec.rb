511a08dee85dca3663a188645334516909b2eccc
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-15 12:54:20 -0800

    test get locations route by params; id, planet and galaxy

--- spec/requests/get_locations_spec.rb
+++ spec/requests/get_locations_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "get all locations route", :type => :request do
-  let!(:location) { FactoryBot.create_list(:location, 10) }
+  let!(:locations) { FactoryBot.create_list(:location, 10) }
 
   before { get '/api/v1/locations', headers: {Authorization: ENV['DROID_SHOP_API_KEY']}}
 
@@ -11,5 +11,26 @@ describe "get all locations route", :type => :request do
 
   it "returns status code 200" do
     expect(response).to have_http_status(:success)
+  end
+
+  context "when given an location id" do
+    it "returns one location" do
+      get "/api/v1/locations/#{locations.first['id']}", headers: {Authorization: ENV['DROID_SHOP_API_KEY']}
+      expect(JSON.parse(response.body)['id']).to eq locations.first['id']
+    end
+  end
+
+  context "when given an location galaxy" do
+    it "returns one location" do
+      get "/api/v1/locations/?galaxy=#{locations.first['galaxy']}", headers: {Authorization: ENV['DROID_SHOP_API_KEY']}
+      expect(JSON.parse(response.body).first['galaxy']).to eq locations.first['galaxy']
+    end
+  end
+
+  context "when given an location planet" do
+    it "returns one location" do
+      get "/api/v1/locations/?planet=#{locations.first['planet']}", headers: {Authorization: ENV['DROID_SHOP_API_KEY']}
+      expect(JSON.parse(response.body).first['planet']).to eq locations.first['planet']
+    end
   end
 end
